6edeff6f29512825ce7485d3001a1b9b3208b43e
Christian Alston <xcda15@gmail.com>
2020-09-25 20:13:28 -0400

    added get and post requests for newly added ejs files

--- app.js
+++ app.js
@@ -27,6 +27,45 @@ app.get('/login', (req, res) => {
     res.render('login')
 })
 
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+app.get('/userPage', (req, res) => {
+    res.render('userPage')
+})
+
+app.get('/hotelResults', (req, res) => {
+    // res.render('hotelResults', results)
+})
+
+app.get('/hotelModal', (req, res) => {
+    res.render('/hotelModal')
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    // res.render('/userPage', data)
+})
+
+app.post('/signup', (req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const username = req.body.username
+    const password = req.body.password
+    // res.render('/userPage')
+})
+
+app.post('/userPage', (req, res) => {
+    const search = req.body.search
+})
+
+app.post('/hotelResults', (req, res) => {
+    const search = req.body.search
+    const filter = req.body.filter
+})
+
 app.listen(PORT, () => {
     console.log(`Server ready on port ${PORT}.`)
 })
